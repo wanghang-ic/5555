9bd2d79076ad1cb022cf4c2b58f45bf38b7ccf88
wanghang-web <2297743840@qq.com>
2020-11-15 21:40:34 +0800

    5555

---
+++ jiegoti.go
@@ -0,0 +1,13 @@
+package main
+
+func main() {
+	type Person struct{
+		Age int
+		Name string
+		Height float64
+	}
+
+
+
+}
+
---
+++ main.go
@@ -0,0 +1,10 @@
+package main//必须要有一个main包
+import  "fmt"
+
+fumc main(){
+   var str1 string//声明变量
+    str1 = "wanghang"
+    fmt.Println("str1 = ",str1)
+
+
+}
